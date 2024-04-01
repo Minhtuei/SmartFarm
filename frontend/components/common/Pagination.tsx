cbaa480e4194374c75f6a01a3ab6394dcbae8709
Minhtuei <tue.tran20032003@gmail.com>
2024-04-02 01:29:18 +0700

    feat(device-page): add implementation of device page

---
+++ frontend/components/common/Pagination.tsx
@@ -0,0 +1,48 @@
+import { ArrowLeftIcon, ArrowRightIcon } from '@heroicons/react/24/outline';
+import { Button, IconButton } from '@material-tailwind/react';
+import { useState } from 'react';
+export function Pagination() {
+    const [active, setActive] = useState<number>(1);
+    interface ItemProps {
+        variant: 'filled' | 'text';
+        color: 'gray';
+        onClick: () => void;
+    }
+    const getItemProps = (index: number): ItemProps => {
+        return {
+            variant: active === index ? 'filled' : 'text',
+            color: active === index ? 'gray' : 'gray',
+            onClick: () => setActive(index)
+        };
+    };
+
+    const next = () => {
+        if (active === 5) return;
+
+        setActive(active + 1);
+    };
+
+    const prev = () => {
+        if (active === 1) return;
+
+        setActive(active - 1);
+    };
+    return (
+        <div className='hidden lg:flex items-center gap-4'>
+            <Button variant='text' className='flex items-center gap-2' onClick={prev} disabled={active === 1}>
+                <ArrowLeftIcon strokeWidth={2} className='h-4 w-4' /> Previous
+            </Button>
+            <div className='flex items-center gap-2'>
+                <IconButton {...getItemProps(1)}>1</IconButton>
+                <IconButton {...getItemProps(2)}>2</IconButton>
+                <IconButton {...getItemProps(3)}>3</IconButton>
+                <IconButton {...getItemProps(4)}>4</IconButton>
+                <IconButton {...getItemProps(5)}>5</IconButton>
+            </div>
+            <Button variant='text' className='flex items-center gap-2' onClick={next} disabled={active === 5}>
+                Next
+                <ArrowRightIcon strokeWidth={2} className='h-4 w-4' />
+            </Button>
+        </div>
+    );
+}
--- frontend/pages/DevicePage.tsx
+++ frontend/pages/DevicePage.tsx
@@ -1,49 +1,218 @@
+import LightSensor from '@fe/assets/light-sensor.png';
 import { AppNavigationBar } from '@fe/components';
+import { AdjustmentsHorizontalIcon, BoltIcon, MagnifyingGlassIcon, PlusIcon, TrashIcon } from '@heroicons/react/20/solid';
+import {
+    Button,
+    Card,
+    CardBody,
+    IconButton,
+    Input,
+    Menu,
+    MenuHandler,
+    MenuItem,
+    MenuList,
+    Switch,
+    Typography
+} from '@material-tailwind/react';
+import { Pagination } from 'frontend/components/common/Pagination';
+
 export function DevicePage() {
     return (
         <>
             <AppNavigationBar title={'Device'} />
-            <div className='p-4 bg-white/2'>
-                <div className=''>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita saepe atque, animi officiis praesentium quis, officia
-                    rem nam molestias quia autem at itaque placeat nihil est exercitationem repellendus illum quam hic fugit cupiditate?
-                    Quisquam veniam beatae excepturi! Fugiat fuga itaque ipsam praesentium perspiciatis, qui vero. Perspiciatis corporis, et
-                    id, nobis quibusdam illum vitae ipsam voluptatibus iste repellendus dicta laboriosam, maiores neque possimus aperiam
-                    necessitatibus? Ex saepe sequi iste, ea, aspernatur placeat consequatur aut officia non eveniet dolores suscipit
-                    architecto voluptate, quibusdam rerum illum quas! Cum tempora animi fugiat suscipit id vel error cupiditate itaque
-                    dignissimos, eveniet iste distinctio provident accusamus? Libero pariatur, enim dolore odit quisquam nemo quae labore
-                    facere? Deserunt, blanditiis! Saepe nam tempora, quos placeat optio dolorem! Labore in beatae nihil odit optio nemo
-                    totam aut temporibus velit impedit, corrupti assumenda dolores dolorum ab aspernatur soluta voluptates hic debitis
-                    veritatis! Incidunt impedit facilis esse amet sunt eaque officia illum dolorem consequuntur ex inventore odio porro,
-                    dicta earum doloremque nobis dolor hic illo quod enim rerum. Optio veniam reiciendis unde hic nostrum commodi assumenda
-                    officia. Magni impedit perspiciatis, est molestiae distinctio corporis harum veritatis. Consequatur repudiandae facilis,
-                    aliquid nisi corrupti officia repellendus atque. Voluptate doloremque amet vero quam maxime porro distinctio, architecto
-                    ipsum inventore illo iste, fugiat, officia quaerat. Officia atque porro voluptatem ullam adipisci molestiae temporibus
-                    eius dolor fugiat modi voluptates dolorem quis sit, culpa, consectetur aut ratione obcaecati ea perferendis architecto
-                    totam! Deserunt est quaerat sequi ea iure rem facilis consequatur hic, nesciunt culpa, laudantium alias totam officiis
-                    atque doloribus! Corporis animi voluptas dolorum architecto neque quam nam doloribus molestias dolor ab totam, cum
-                    quibusdam quae quod cupiditate molestiae iste omnis quasi maiores obcaecati ex nobis fuga doloremque tempora. Reiciendis
-                    vitae repellendus et quisquam nihil deserunt omnis voluptate, consectetur vero libero sunt delectus possimus in? Rerum
-                    eaque, voluptatum nobis cupiditate similique distinctio asperiores nisi deserunt totam quidem placeat ex reiciendis
-                    perspiciatis ducimus ea consequuntur quam quis ullam fugiat culpa laboriosam velit? Omnis a eius suscipit. Voluptates
-                    quibusdam nihil praesentium doloribus impedit ipsa temporibus qui! Odio corporis soluta voluptatum error, dolor harum
-                    consectetur tenetur eius illum voluptatibus assumenda facere debitis, recusandae veniam at. Commodi totam vitae saepe
-                    corrupti possimus quo illum deserunt eligendi quidem, debitis ea repellat consectetur at, laboriosam, architecto sequi
-                    repellendus hic perspiciatis. Reprehenderit veritatis iste, nemo a excepturi necessitatibus amet pariatur esse eius
-                    facere voluptatem soluta est velit earum perferendis, enim sed provident praesentium ad aspernatur sunt fugit? Vero fuga
-                    ullam minus. Perspiciatis vitae nostrum assumenda cumque quaerat dolorum ipsam dignissimos magnam! Accusamus deleniti
-                    qui optio veritatis nam ipsum, inventore esse facilis nostrum fuga voluptatem aspernatur corrupti officia voluptas nulla
-                    facere rem suscipit itaque quaerat doloribus alias odio, ab in sit! Quis ab provident blanditiis voluptatibus beatae
-                    impedit ipsa? Corporis, libero asperiores! Ab saepe aliquam quasi, aspernatur aut illo explicabo, consequatur veritatis
-                    consectetur error hic minima voluptate consequuntur laudantium odit quisquam! Dolorem quibusdam enim ipsa minima vel
-                    molestias maiores dolores neque repellendus mollitia blanditiis iste odit provident beatae distinctio atque consequatur
-                    magni, similique magnam a? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt dolor voluptates nobis dicta
-                    magni ex, exercitationem cumque porro aliquid aspernatur praesentium harum perferendis quaerat vitae consequuntur omnis
-                    quae. Odit iusto nam enim illum magni. Velit beatae necessitatibus quae exercitationem error amet perspiciatis molestias
-                    aperiam incidunt omnis. Sapiente, at. Incidunt laudantium iusto, quas rerum aspernatur facere possimus voluptatibus odit
-                    soluta impedit voluptates, nulla in nisi ab eos minus ad ipsum illo molestias blanditiis. Assumenda dolorem itaque, quo
-                    adipisci, quam odio sed asperiores iure facilis sapiente, facere corrupti inventore minima? Quam voluptatem error
-                    perspiciatis ut deserunt minima veniam cumque repellat molestias non.
+            <div className='px-8 py-6 bg-white/2 dark:text-white/2 dark:bg-gray-700 '>
+                <div className='h-[calc(100vh-130px)] flex flex-col overflow-hidden relative gap-y-2 overflow-y-auto lg:overflow-hidden'>
+                    <div className='flex items-center gap-2'>
+                        <Menu>
+                            <MenuHandler>
+                                <IconButton placeholder={''} className='px-3 py-2 bg-green/1 '>
+                                    <AdjustmentsHorizontalIcon className='w-6 h-6' />
+                                </IconButton>
+                            </MenuHandler>
+                            <MenuList placeholder={''}>
+                                <MenuItem placeholder={''}>Menu Item 1</MenuItem>
+                                <MenuItem placeholder={''}>Menu Item 2</MenuItem>
+                                <MenuItem placeholder={''}>Menu Item 3</MenuItem>
+                            </MenuList>
+                        </Menu>
+                        <Input
+                            type='device'
+                            placeholder='Search for a device...'
+                            className='!border !border-gray-300 bg-white text-gray-900 shadow-lg shadow-gray-900/5 ring-4 ring-transparent placeholder:text-gray-500 placeholder:opacity-100 focus:!border-gray-900 focus:!border-t-gray-900 focus:ring-gray-900/10'
+                            labelProps={{
+                                className: 'hidden'
+                            }}
+                            containerProps={{ className: 'min-w-[100px] w-1/4' }}
+                            crossOrigin={'none'}
+                        />{' '}
+                        <IconButton color='green' placeholder={''} className='px-3 py-2 hover:shadow-none '>
+                            <MagnifyingGlassIcon className='w-6 h-6' />
+                        </IconButton>
+                        <IconButton color='blue' placeholder={''} className='px-3 py-2 hover:shadow-none '>
+                            <PlusIcon className='w-6 h-6' />
+                        </IconButton>
+                        <IconButton color='red' placeholder={''} className='px-3 py-2 hover:shadow-none '>
+                            <TrashIcon className='w-6 h-6' />
+                        </IconButton>
+                    </div>
+                    <div className='flex flex-wrap justify-between gap-y-4'>
+                        <Card className='w-[400px] cursor-pointer shrink'>
+                            <CardBody className='flex items-center'>
+                                <BoltIcon className='w-8 h-8 bg-blue-100 rounded-full p-1' />
+                                <Typography color='blue' className='ml-2' placeholder={''}>
+                                    Thiết bị cảm biến ánh sáng
+                                </Typography>
+                                <div className=' flex ml-auto'>
+                                    <Switch color='blue' defaultChecked />
+                                </div>
+                            </CardBody>
+                            <CardBody className='flex items-center py-0'>
+                                <div className='w-[100px] h-[100px] shrink-0'>
+                                    <img src={LightSensor} alt='device' className='w-full h-full object-cover' />
+                                </div>
+                                <div className='flex flex-1 gap-1'>
+                                    <div className='flex flex-col gap-1'>
+                                        <Typography className='text-md font-semibold' placeholder={''}>
+                                            Tên thiết bị:
+                                        </Typography>
+                                        <Typography className='text-md font-semibold' placeholder={''}>
+                                            Mã thiết bị:
+                                        </Typography>
+                                        <Typography className='text-md font-semibold' placeholder={''}>
+                                            Trạng thái:
+                                        </Typography>
+                                    </div>
+                                    <div className='flex flex-col gap-1'>
+                                        <Typography className='text-md' placeholder={''}>
+                                            Cảm biến ánh sáng
+                                        </Typography>
+                                        <Typography className='text-md' placeholder={''}>
+                                            123456
+                                        </Typography>
+                                        <Typography color='green' className='text-md' placeholder={''}>
+                                            Đang hoạt động
+                                        </Typography>
+                                    </div>
+                                </div>
+                            </CardBody>
+                            <CardBody className='flex items-center gap-2'>
+                                <Button variant='outlined' color='blue' size='sm'>
+                                    Xem chi tiết
+                                </Button>
+                                <Button variant='outlined' color='red' size='sm'>
+                                    Xóa thiết bị
+                                </Button>
+                            </CardBody>
+                        </Card>
+
+                        <Card className='w-[400px] cursor-pointer shrink'>
+                            <CardBody className='flex items-center'>
+                                <BoltIcon className='w-8 h-8 bg-blue-100 rounded-full p-1' />
+                                <Typography color='blue' className='ml-2' placeholder={''}>
+                                    Thiết bị cảm biến ánh sáng
+                                </Typography>
+                                <div className=' flex ml-auto'>
+                                    <Switch color='blue' defaultChecked />
+                                </div>
+                            </CardBody>
+                            <CardBody className='flex items-center py-0'>
+                                <div className='w-[100px] h-[100px] shrink-0'>
+                                    <img src={LightSensor} alt='device' className='w-full h-full object-cover' />
+                                </div>
+                                <div className='flex flex-1 gap-1'>
+                                    <div className='flex flex-col gap-1'>
+                                        <Typography className='text-md font-semibold' placeholder={''}>
+                                            Tên thiết bị:
+                                        </Typography>
+                                        <Typography className='text-md font-semibold' placeholder={''}>
+                                            Mã thiết bị:
+                                        </Typography>
+                                        <Typography className='text-md font-semibold' placeholder={''}>
+                                            Trạng thái:
+                                        </Typography>
+                                    </div>
+                                    <div className='flex flex-col gap-1'>
+                                        <Typography className='text-md' placeholder={''}>
+                                            Cảm biến ánh sáng
+                                        </Typography>
+                                        <Typography className='text-md' placeholder={''}>
+                                            123456
+                                        </Typography>
+                                        <Typography color='green' className='text-md' placeholder={''}>
+                                            Đang hoạt động
+                                        </Typography>
+                                    </div>
+                                </div>
+                            </CardBody>
+                            <CardBody className='flex items-center gap-2'>
+                                <Button variant='outlined' color='blue' size='sm'>
+                                    Xem chi tiết
+                                </Button>
+                                <Button variant='outlined' color='red' size='sm'>
+                                    Xóa thiết bị
+                                </Button>
+                            </CardBody>
+                        </Card>
+                        <Card className='w-[400px] cursor-pointer shrink'>
+                            <CardBody className='flex items-center'>
+                                <BoltIcon className='w-8 h-8 bg-blue-100 rounded-full p-1' />
+                                <Typography color='blue' className='ml-2' placeholder={''}>
+                                    Thiết bị cảm biến ánh sáng
+                                </Typography>
+                                <div className=' flex ml-auto'>
+                                    <Switch color='blue' defaultChecked />
+                                </div>
+                            </CardBody>
+                            <CardBody className='flex items-center py-0'>
+                                <div className='w-[100px] h-[100px] shrink-0'>
+                                    <img src={LightSensor} alt='device' className='w-full h-full object-cover' />
+                                </div>
+                                <div className='flex flex-1 gap-1'>
+                                    <div className='flex flex-col gap-1'>
+                                        <Typography className='text-md font-semibold' placeholder={''}>
+                                            Tên thiết bị:
+                                        </Typography>
+                                        <Typography className='text-md font-semibold' placeholder={''}>
+                                            Mã thiết bị:
+                                        </Typography>
+                                        <Typography className='text-md font-semibold' placeholder={''}>
+                                            Trạng thái:
+                                        </Typography>
+                                    </div>
+                                    <div className='flex flex-col gap-1'>
+                                        <Typography className='text-md' placeholder={''}>
+                                            Cảm biến ánh sáng
+                                        </Typography>
+                                        <Typography className='text-md' placeholder={''}>
+                                            123456
+                                        </Typography>
+                                        <Typography color='green' className='text-md' placeholder={''}>
+                                            Đang hoạt động
+                                        </Typography>
+                                    </div>
+                                </div>
+                            </CardBody>
+                            <CardBody className='flex items-center gap-2'>
+                                <Button variant='outlined' color='blue' size='sm'>
+                                    Xem chi tiết
+                                </Button>
+                                <Button variant='outlined' color='red' size='sm'>
+                                    Xóa thiết bị
+                                </Button>
+                            </CardBody>
+                        </Card>
+                        <Card className='w-[400px] flex items-center justify-center cursor-pointer shrink'>
+                            <CardBody className='flex flex-col items-center'>
+                                <PlusIcon className='w-14 h-14'></PlusIcon>
+                                <Typography className='ml-2' placeholder={''}>
+                                    Thêm thiết bị
+                                </Typography>
+                            </CardBody>
+                        </Card>
+                    </div>
+                    <div className='absolute bottom-0 flex left-1/2 -translate-x-1/2'>
+                        <Pagination />
+                    </div>
                 </div>
             </div>
         </>
