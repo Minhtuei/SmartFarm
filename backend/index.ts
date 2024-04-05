903b5d79655db7b483fc7da51374e5985ceb4731
tungdoanduy <sion080803@gmail.com>
2024-04-05 09:42:51 +0700

    refactor(index) : add router of otp

--- backend/index.ts
+++ backend/index.ts
@@ -8,6 +8,7 @@ import { router as login } from './routes/auth/authRouter';
 import { router as user } from './routes/userRouter';
 import { router as device } from './routes/deviceRouter';
 import { router as notification } from './routes/notificationRouter';
+import { router as otp } from './routes/genAccRouter';
 import { authenticate } from './services/authenticate';
 const session = require('express-session');
 
@@ -50,6 +51,7 @@ try {
     app.use('/user', authenticate, user);
     app.use('/device', authenticate, device);
     app.use('/notification', authenticate, notification);
+    app.use('/otp', otp);
 } catch (err) {
     console.error('fix: ------\n' + err + '\n-------------------\n');
 }
