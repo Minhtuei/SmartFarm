dacd843b33585d6dd15faa24edb9ecc021cecd4c
tungdoanduy <sion080803@gmail.com>
2024-04-14 10:08:28 +0700

    refactor(notification) : add check valid

--- backend/controllers/notificationController.ts
+++ backend/controllers/notificationController.ts
@@ -3,30 +3,12 @@ import { StatusCodes } from 'http-status-codes';
 import { Request, Response } from 'express';
 import { validateEmail } from '@fe/utils';
 import { User } from '@be/models';
+import { Device } from '../models/device';
 import { SendNotification } from './emailController';
 
 export const getAllNotification = async (req: Request, res: Response) => {
     try {
-        const notifications = await Notification.find({ email: req.params.email });
-        res.status(StatusCodes.OK).json({ notifications });
-    } catch (error) {
-        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: 'Internal Server Error' });
-    }
-};
-
-export const getNotificationByDevice = async (req: Request, res: Response) => {
-    try {
-        const notifications = await Notification.find({ email: req.params.email, deviceName: req.params.deviceName });
-        if (!notifications) return res.status(StatusCodes.OK).json({ message: "Don't have notifications!" });
-        return res.status(StatusCodes.OK).json({ notifications });
-    } catch (error) {
-        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: 'Internal Server Error' });
-    }
-};
-
-export const createNotification = async (req: Request, res: Response) => {
-    try {
-        const { email } = req.body;
+        const email = req.params.email;
         if (!email) {
             return res.status(StatusCodes.BAD_REQUEST).json({
                 status: 'error',
@@ -41,8 +23,71 @@ export const createNotification = async (req: Request, res: Response) => {
         }
         const user = await User.findOne({ email: email });
         if (!user) {
-            return res.status(StatusCodes.UNAUTHORIZED).json({ message: 'User not found' });
+            return res.status(StatusCodes.NOT_FOUND).json({ message: 'User not found' });
         }
+        const notifications = await Notification.find({ email: email });
+        res.status(StatusCodes.OK).json({ notifications });
+    } catch (error) {
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: 'Internal Server Error' });
+    }
+};
+
+export const getNotificationByDevice = async (req: Request, res: Response) => {
+    try {
+        const email = req.params.email;
+        const deviceName = req.params.deviceName;
+        if (!email) {
+            return res.status(StatusCodes.BAD_REQUEST).json({
+                status: 'error',
+                message: 'Missing email!'
+            });
+        }
+        if (!validateEmail(email)) {
+            return res.status(StatusCodes.BAD_REQUEST).json({
+                status: 'error',
+                message: 'Invalid email!'
+            });
+        }
+        const user = await User.findOne({ email: email });
+        if (!user) {
+            return res.status(StatusCodes.NOT_FOUND).json({ message: 'User not found' });
+        }
+        const deviceNeeded = await Device.findOne({ userID: user._id, deviceName: deviceName });
+        if (!deviceNeeded) {
+            return res.status(StatusCodes.NOT_FOUND).json({ message: 'Device not found' });
+        }
+        const notifications = await Notification.find({ email: email, deviceName: deviceName });
+        if (!notifications) return res.status(StatusCodes.OK).json({ message: "Don't have notifications!" });
+        return res.status(StatusCodes.OK).json({ notifications });
+    } catch (error) {
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: 'Internal Server Error' });
+    }
+};
+
+export const createNotification = async (req: Request, res: Response) => {
+    try {
+        const { email, deviceName } = req.body;
+        if (!email) {
+            return res.status(StatusCodes.BAD_REQUEST).json({
+                status: 'error',
+                message: 'Missing email!'
+            });
+        }
+        if (!validateEmail(email)) {
+            return res.status(StatusCodes.BAD_REQUEST).json({
+                status: 'error',
+                message: 'Invalid email!'
+            });
+        }
+        const user = await User.findOne({ email: email });
+        if (!user) {
+            return res.status(StatusCodes.NOT_FOUND).json({ message: 'User not found' });
+        }
+        const deviceNeeded = await Device.findOne({ userID: user._id, deviceName: deviceName });
+        if (!deviceNeeded) {
+            return res.status(StatusCodes.NOT_FOUND).json({ message: 'Device not found' });
+        }
+
         const notification = await Notification.create(req.body);
         const time = notification.createdAt;
         await SendNotification(req, res);
